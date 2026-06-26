adf8889e785ac5227223c6576039d66c88eab9f1
Airshock72 <gelagela2222@gmail.com>
2026-06-26 15:59:35 +0400

    add Villa entity and configure ApplicationDbContext for EF Core

--- backend/Bulky/Bulky.Infrastructure/Data/ApplicationDbContext.cs
+++ backend/Bulky/Bulky.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Bulky.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bulky.Infrastructure.Data;
 
@@ -7,4 +8,6 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
     }
+    
+    public DbSet<Villa> Villas { get; set; }
 }
