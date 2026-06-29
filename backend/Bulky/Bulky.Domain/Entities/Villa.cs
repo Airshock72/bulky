7ca3ac02387eba947c79f591e20f09d9367e16ed
Airshock72 <gelagela2222@gmail.com>
2026-06-29 17:21:50 +0400

    add validation data annotations to Villa entity
    - Add `MaxLength` attributes to `Name` and `Description` properties.
    - Add `Range` attributes to `Price`, `Sqft`, and `Occupancy` properties.
    - Import `System.ComponentModel.DataAnnotations` namespace in `Villa.cs`.

--- backend/Bulky/Bulky.Domain/Entities/Villa.cs
+++ backend/Bulky/Bulky.Domain/Entities/Villa.cs
@@ -1,12 +1,19 @@
-﻿namespace Bulky.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Domain.Entities;
 
 public class Villa
 {
     public int Id { get; set; }
+    [MaxLength(100)]
     public required string Name { get; set; }
+    [MaxLength(250)]
     public string? Description { get; set; }
+    [Range(10, 10000)]
     public double Price { get; set; }
+    [Range(10, 10000)]
     public int Sqft { get; set; }
+    [Range(1, 100)]
     public int Occupancy { get; set; }
     public string? ImageUrl { get; set; }
     public DateTime? CreatedDate { get; set; }
