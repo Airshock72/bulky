18a5da32e0735c89514c88d982a2cbec3a8117de
Airshock72 <gelagela2222@gmail.com>
2026-06-29 16:57:15 +0400

    add Create endpoint and refactor VillaController
    - Refactor `VillaController` to use C# primary constructor syntax for `ApplicationDbContext`.
    - Add `Create` POST action to add new villas to the database and return the created ID.

--- backend/Bulky/Bulky.Web/Controllers/VillaController.cs
+++ backend/Bulky/Bulky.Web/Controllers/VillaController.cs
@@ -6,15 +6,20 @@ namespace BulkyWeb.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class VillaController : ControllerBase
+public class VillaController(ApplicationDbContext db) : ControllerBase
 {
-    private readonly ApplicationDbContext _db;
-    public VillaController(ApplicationDbContext db) { _db = db; }
-
     [HttpGet]
     public IActionResult GetAll()
     {
-        List<Villa> villas = _db.Villas.ToList();
+        List<Villa> villas = db.Villas.ToList();
         return Ok(villas);
+    }
+
+    [HttpPost]
+    public IActionResult Create(Villa obj)
+    {
+        db.Villas.Add(obj);
+        db.SaveChanges();
+        return Ok(obj.Id);
     }
 }
