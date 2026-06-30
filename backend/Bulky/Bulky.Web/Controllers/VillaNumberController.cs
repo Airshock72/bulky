c53cb4af0d389079ac295173aa685e70475bf36b
Airshock72 <gelagela2222@gmail.com>
2026-06-30 16:52:03 +0400

    implement VillaNumberController for Bulky.Web
    - Add `VillaNumberController` with RESTful endpoints for managing villa numbers.
    - Implement CRUD operations: `GetAll`, `Create`, `Update`, and `Delete`.
    - Add validation logic to verify the existence of a `Villa` before creating a `VillaNumber`.
    - Integrate `ApplicationDbContext` for data persistence.

---
+++ backend/Bulky/Bulky.Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,57 @@
+using Bulky.Domain.Entities;
+using Bulky.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class VillaNumberController : ControllerBase
+{
+    private readonly ApplicationDbContext _db;
+    public VillaNumberController(ApplicationDbContext db) { _db = db; }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        List<VillaNumber> villaNumbers = _db.VillaNumbers.ToList();
+        return Ok(villaNumbers);
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] VillaNumber obj)
+    {
+        bool villaExist = _db.Villas.Any(v => v.Id == obj.VillaId);
+        if (!villaExist) return NotFound();
+        
+        _db.VillaNumbers.Add(obj);
+        _db.SaveChanges();
+        return Ok(obj.Id);
+    }
+
+    [HttpPut("{villaNumberId:int}")]
+    public IActionResult Update(int villaNumberId, [FromBody] VillaNumber updatedVillaNumber)
+    {
+        VillaNumber? villaNumber = _db.VillaNumbers.FirstOrDefault(vn => vn.Id == villaNumberId);
+        if (villaNumber == null) return NotFound();
+        
+        villaNumber.Number = updatedVillaNumber.Number;
+        villaNumber.SpecialDetails = updatedVillaNumber.SpecialDetails;
+        villaNumber.VillaId = updatedVillaNumber.VillaId;
+
+        _db.SaveChanges();
+        return Ok(villaNumber);
+    }
+
+    [HttpDelete("{villaNumberId:int}")]
+    public IActionResult Delete(int villaNumberId)
+    {
+        VillaNumber? villaNumber = _db.VillaNumbers.FirstOrDefault(vn => vn.Id == villaNumberId);
+
+        if (villaNumber == null) return NotFound();
+
+        _db.VillaNumbers.Remove(villaNumber);
+        _db.SaveChanges();
+        return Ok();
+    }
+}
